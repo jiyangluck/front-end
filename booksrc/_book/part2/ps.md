cc120c67e4bbd56837842556b161e0d7ffda6df2
jiyangluck <287987435@qq.com>
2016-10-07 14:56:56 +0800

    Update 2016-10-07T06:56:36.227Z

--- booksrc/_book/part2/ps.md
+++
@@ -1,2 +0,0 @@
-# 2-Photoshop基础
-
--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var ghPages = require('gulp-gh-pages');
-
-gulp.task('default', function() {
-  return gulp.src('./booksrc/_book/**')
-    .pipe(ghPages());
-});
